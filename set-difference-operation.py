a4d82b876606b3b7c5c133e034eeac28fb55733c
Bereket Tadesse <106895952+berek47@users.noreply.github.com>
2024-04-10 23:47:59 +0300

    a2sv

---
+++ set-difference-operation.py
@@ -0,0 +1,8 @@
+no_english = int(input())
+english_students = set(map(int, input().split()))
+
+no_french = int(input())
+french_students = set(map(int, input().split()))
+
+unique_english_students = len(english_students.difference(french_students))
+print(unique_english_students)
